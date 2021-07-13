7fe92c8c35d5d450449d4891295115bf83c455af
Дмитрий Гришин <DIGrishin@alfabank.ru>
2021-07-13 18:01:39 +0300

    specialities

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  axios.get("http://priem2021.spmi.ru/list/public/data/naprBakalavrBudjet.json")
-      .then(response => {
-        console.log(response)
-        res.render('index', { subjects: response.data })
-      })
+    axios.all([
+        axios.get("http://priem2021.spmi.ru/list/public/data/naprBakalavrBudjet.json"),
+        axios.get("http://priem2021.spmi.ru/list/public/data/naprSpecialistBudjet.json")
+    ]).then(results => {
+        const subjects = results[0].data.concat(results[1].data)
+        res.render('index', { subjects: subjects })
+    })
 });
 
 module.exports = router;
